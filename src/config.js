0619916956fc689fb15e9051a8c166f7eb34b090
simon0712hll <851592974@qq.com>
2024-05-25 17:08:47 +0800

    修改生产环境Ip

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL: process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : 'http://localhost:5000'
+    BASE_URL: process.env.NODE_ENV === 'production' ? 'http://10.105.183.67:5000' : 'http://localhost:5000'
   };
   
   export default config;
