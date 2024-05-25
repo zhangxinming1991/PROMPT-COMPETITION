253c4e0d3888d7282c3ad8c492ad739784de443a
simon0712hll <851592974@qq.com>
2024-05-25 16:10:27 +0800

    修改前台服务安装脚本

--- install/install_frontend_service.bat
+++ install/install_frontend_service.bat
@@ -1,8 +1,8 @@
 @echo off
 SET SERVICE_NAME=PromptCompetitionService
 SET WORKING_DIR=D:\MyProject\PROMPT-COMPETITION
-SET SERVE_CMD="D:\Program Files\nodejs\npx.cmd"
-SET SERVE_ARGS=serve -s build -l 3001
+SET SERVE_CMD="D:\MyProject\PROMPT-COMPETITION\node_modules\.bin\serve.cmd"
+SET SERVE_ARGS=-s build -l 3001
 SET NSSM_PATH="D:\Program Files\nssm-2.24\nssm-2.24\win64\nssm.exe"
 SET LOG_DIR=D:\applog\frontend_service
 
